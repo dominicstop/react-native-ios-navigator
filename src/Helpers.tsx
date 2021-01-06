7d310e9f5bdaa6c18164fe3d4e28a34d667fb3ce
Dominic Go <dominicstop@gmail.com>
2021-01-06 17:17:10 +0800

    ✨ Minor Changes: Helpers - Updated types

--- src/Helpers.tsx
+++ src/Helpers.tsx
@@ -1,23 +1,23 @@
 import type React from 'react';
 
-//wrapper func for setState that returns a promise
+// wrapper func for setState that returns a promise
 export function setStateAsync<T>(
   that: React.Component,
   newState: T | ((prevState: T) => T)
-) {
-  return new Promise((resolve) => {
+){
+  return new Promise<void>((resolve) => {
     that.setState(newState, () => {
-      resolve(null);
+      resolve();
     });
   });
 };
 
-//wrapper for timeout that returns a promise
+// wrapper for timeout that returns a promise
 export function timeout(ms: Number) {
-  return new Promise(resolve => {
+  return new Promise<void>(resolve => {
     const timeoutID = setTimeout(() => {
       clearTimeout(timeoutID);
-      resolve(null);
+      resolve();
     }, ms)
   });
 };
