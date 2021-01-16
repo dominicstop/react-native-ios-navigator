12df7585c3aa84f1383688431ba088188794d3f6
Dominic Go <dominicstop@gmail.com>
2021-01-17 00:59:43 +0800

    🆕 Added: `useNavRouteEvents`

---
+++ src/hooks/useNavRouteEvents.ts
@@ -0,0 +1,34 @@
+import { useContext, useEffect, useRef } from 'react';
+
+import type { NavRouteEvents } from '../components/NavigatorRouteView';
+import { NavRouteViewContext } from '../context/NavRouteViewContext';
+
+
+export function useNavRouteEvents(eventName: NavRouteEvents, handler: () => void){
+  const { getEmitterRef } = useContext(NavRouteViewContext);
+  
+  // Create a ref that stores handler
+  const savedHandler = useRef(null);
+
+  // Update ref.current value if handler changes.
+  // This allows our effect below to always get latest handler ...
+  // ... without us needing to pass it in effect deps array ...
+  // ... and potentially cause effect to re-run every render.
+  useEffect(() => {
+    savedHandler.current = handler;
+  }, [handler]);
+
+  useEffect(() => {
+    const emitterRef = getEmitterRef();
+
+    // create event listener that calls handler function stored in ref
+    const eventListener = (params) => savedHandler.current(params);
+    // subscribe to events
+    emitterRef.addListener(eventName, eventListener);
+    
+    return () => {
+      // remove event listener on cleanup
+      emitterRef.removeListener(eventName, eventListener);
+    };
+  });
+};
--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,5 @@ export * from './components/NavigatorRouteView';
 export * from './components/NavigatorView';
 
 export * from './hoc/withRouteViewLifecycle';
+export * from './hooks/useNavRouteEvents';
 export * from './context/NavRouteViewContext';
