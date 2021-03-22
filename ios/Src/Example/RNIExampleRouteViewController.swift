6bb33368965879446a41c1770b06da70252df373
Dominic Go <dominicstop@gmail.com>
2021-03-23 00:50:30 +0800

    💫 Updated: `RNIExampleRouteViewController`

--- ios/Src/Example/RNIExampleRouteViewController.swift
+++ ios/Src/Example/RNIExampleRouteViewController.swift
@@ -5,13 +5,45 @@
 //  Created by Dominic Go on 3/15/21.
 //
 
-import Foundation
+import UIKit
 
  
 class RNIExampleRouteViewController: RNINavigatorRouteBaseViewController {
   override func loadView() {
     super.loadView();
     
-    self.view.backgroundColor = .blue;
+    self.navigationItem.title = "Native Route";
+    
+    let title = UILabel();
+    title.text = "Native Route: \(self.routeKey)";
+    title.font = .systemFont(ofSize: 18, weight: .bold);
+    
+    let subtitle = UILabel();
+    subtitle.text = "Route Index: \(self.routeIndex)";
+    subtitle.font = .systemFont(ofSize: 16, weight: .regular);
+    
+    let stack = UIStackView(arrangedSubviews: [title, subtitle]);
+    stack.layer.cornerRadius = 10;
+    
+    if #available(iOS 11.0, *) {
+      stack.isLayoutMarginsRelativeArrangement = true
+      stack.directionalLayoutMargins =
+        NSDirectionalEdgeInsets(top: 15, leading: 15, bottom: 15, trailing: 15);
+    };
+
+    stack.axis = .vertical;
+    stack.alignment = .center;
+    stack.backgroundColor = .lightGray;
+    
+    
+    self.view.backgroundColor = .white;
+    
+    self.view.addSubview(stack);
+    stack.translatesAutoresizingMaskIntoConstraints = false;
+    
+    NSLayoutConstraint.activate([
+      stack.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+      stack.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+    ]);
   };
 };
