8930cadcfa0e17e271c9d0b09c30d560964da4dc
Dominic Go <dominicstop@gmail.com>
2021-03-27 17:25:36 +0800

    💫 Example - Updated `App`

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -27,63 +27,106 @@ const RouteKeys = {
 
 const RouteItems = [{ 
   routeKey: RouteKeys.NavigatorExample01,
+  desc: 'Nested navigator example',
 }, {
   routeKey: RouteKeys.NavigatorTest01,
+  desc: (
+      `Test for all the different ways a navigator bar can be configured,`
+    + ` e.g. this is a test for setting/updating the 'RouteOptions'.`
+  ),
 }, {
   routeKey: RouteKeys.NavigatorTest02,
+  desc: (
+      `Test for showing a custom nav. bar title item and testing whether it`
+    + ` updates property when the props updates.`
+  )
 }, { 
   routeKey: RouteKeys.NavigatorTest03,
+  desc: (
+      `Tester for all the navigation commands currently supported, e.g.`
+    + ` push, pop, 'popToRoot', etc.`
+  )
 }, { 
   routeKey: RouteKeys.NavigatorDemo01,
+  desc: (
+      `Automatic demo for showing nested navigators in varying configurations`
+    + ` any layout. Also tests whether the 'push'/'pop' can be chained together`
+    + ' via async/await + nav. events.'
+  )
 }, { 
   routeKey: RouteKeys.NavigatorDemo02,
+  desc: (
+      `Automatic demo for showing nested navigators, specifically this demos`
+    + ` a customized nav. bar and tests whether they layout properly when they're`
+    + ` continuously stacked/nested on top of one another.`
+    + ` This also tests functional usage + hooks.`
+  )
 }];
+
+
+function RouteItem(props: {
+  routeKey: string;
+  desc: string;
+  index: number;
+  onPress: Function;
+}){
+  return (
+    <View style={styles.routeItemContainer}>
+      <View style={styles.routeItemTitleContainer}>
+        <Text style={styles.textRouteItemTitleBullet}>
+          {`${props.index + 1}.`}
+        </Text>
+        <Text style={styles.textRouteItemTitle}>
+          {props.routeKey}
+        </Text>
+      </View>
+      <View style={styles.routeItemContentContainer}>
+        <Text style={styles.routeItemDescText}>
+          {props.desc}
+        </Text>
+        <TouchableOpacity
+          onPress={() => {
+            props.onPress(props.routeKey);
+          }}
+          style={styles.routeItemButton}
+        >
+          <Text style={styles.routeItemButtonText}>
+            {'Go to Route'}
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
 
 
 class HomeRoute extends React.PureComponent<RouteContentProps> {
   static styles = StyleSheet.create({
     rootContainer: {
-      flex: 1,
-    },
-    navRouteItem: {
-      paddingHorizontal: 10,
-      paddingVertical: 15,
-    },
-    navRouteContainer: {
-    },
-    textNavRouteTitleIndicator: {
-      color: Colors.GREY[900],
-    },
-    textNavRouteTitle: {
-      fontSize: 16,
-      color: Colors.GREY[700],
+      paddingBottom: 100,
     },
   });
 
   // @ts-ignore
-  _handleOnPressItem = async ({item, index}) => {
+  _handleOnPressItem = async (routeKey: string) => {
     const navigation = this.props.navigation;
-    await navigation.push({routeKey: item.routeKey});
+    await navigation.push({routeKey});
   };
 
-  // @ts-ignore
-  _renderItem = ({item, index}) => {
+  _renderItem = ({item, index}: {
+    item: typeof RouteItems[number];
+    index: number;
+  }) => {
     const { styles } = HomeRoute;
 
     return (
-      <TouchableOpacity 
-        style={styles.navRouteItem}
-        onPress={() => this._handleOnPressItem({item, index})}
-      >
-        <View style={styles.navRouteContainer}>
-          <Text style={styles.textNavRouteTitle}>
-            <Text style={styles.textNavRouteTitleIndicator}>
-              {`${index + 1}. `}
-            </Text>
-            {item.routeKey}
-          </Text>
-        </View>
-      </TouchableOpacity>
+      <RouteItem
+        key={item.routeKey}
+        routeKey={item.routeKey}
+        desc={item.desc}
+        index={index}
+        onPress={this._handleOnPressItem}
+      />
     );
   };
 
@@ -93,7 +136,7 @@ class HomeRoute extends React.PureComponent<RouteContentProps> {
     return (
       <React.Fragment>
         <FlatList
-          style={styles.rootContainer}
+          contentContainerStyle={styles.rootContainer}
           data={RouteItems}
           keyExtractor={(item) => item.routeKey}
           renderItem={this._renderItem}
@@ -164,5 +207,50 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  routeItemContainer: {
+    overflow: 'hidden',
+    borderRadius: 10,
+    marginTop: 15,
+    marginHorizontal: 12,
+    backgroundColor: Colors.PURPLE[50],
+  },
+  routeItemTitleContainer: {
+    flexDirection: 'row',
+    backgroundColor: Colors.PURPLE.A400,
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+    alignItems: 'center',
+  },
+  routeItemContentContainer: {
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+  },
+  textRouteItemTitleBullet: {
+    color: 'white',
+    marginRight: 7,
+    fontWeight: 'bold',
+  },
+  textRouteItemTitle: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 17,
+  },
+  routeItemDescText: {
+    color: Colors.PURPLE[1100],
+    opacity: 0.9,
+    fontWeight: '300',
+  },
+  routeItemButton: {
+    backgroundColor: Colors.PURPLE.A700,
+    borderRadius: 10,
+    marginTop: 15,
+    paddingVertical: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  routeItemButtonText: {
+    color: 'white',
+    fontWeight: 'bold',
   },
 });
