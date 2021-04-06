9a785ba169ae992a7f4530b69f8587fbffe4918a
Dominic Go <dominicstop@gmail.com>
2021-04-07 04:02:29 +0800

    💄 Cleanup

--- example/ios/Example/NavigatorManagerListener.swift
+++ example/ios/Example/NavigatorManagerListener.swift
@@ -9,16 +9,21 @@ import Foundation
 import react_native_ios_navigator
 
 
-@objc class NavigatorManagerListener: NSObject, RNINavigatorManagerDelegate {
+@objc class NavigatorManagerListener: NSObject {
   static var sharedInstance: NavigatorManagerListener? = nil;
   
+  /// For some reason, on an objc + swift project, `{}()` syntax isn't evaluated,
+  /// so we have to explicitly set the singleton via invoking this func from objc.
+  /// If you update the `AppDelegate` to use swift, maybe this isn't needed.
   @objc static func setup(){
     let instance = NavigatorManagerListener();
     RNINavigatorManager.sharedInstance.delegate = instance;
     
     Self.sharedInstance = instance;
   };
-  
+};
+
+extension NavigatorManagerListener: RNINavigatorManagerDelegate {
   func onNavigatorViewAdded(_ navigatorView: RNINavigatorView, _ navigatorID: Int) {
     print("navigatorView added w/ navigatorID: \(navigatorID)");
   };
