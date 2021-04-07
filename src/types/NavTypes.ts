524582a08f5437a3494ebcb4790e889b7db7ca1f
Dominic Go <dominicstop@gmail.com>
2021-04-07 08:41:52 +0800

    🛠 Types - Updated `NavigationObject`

--- src/types/NavTypes.ts
+++ src/types/NavTypes.ts
@@ -37,38 +37,38 @@ export type RouteOptions = {
 };
 
 export type NavigationObject = {
-  routeKey  ?: string;
-  routeIndex?: number;
+  routeKey   : string;
+  routeIndex : number;
   routeProps?: object;
 
   // navigator commands
-  push        ?: typeof NavigatorView.prototype.push;
-  pop         ?: typeof NavigatorView.prototype.pop;
-  popToRoot   ?: typeof NavigatorView.prototype.popToRoot;
-  removeRoute ?: typeof NavigatorView.prototype.removeRoute;
-  removeRoutes?: typeof NavigatorView.prototype.removeRoutes;
-  replaceRoute?: typeof NavigatorView.prototype.replaceRoute;
-  insertRoute ?: typeof NavigatorView.prototype.insertRoute;
-  setRoutes   ?: typeof NavigatorView.prototype.setRoutes;
+  push        : typeof NavigatorView.prototype.push;
+  pop         : typeof NavigatorView.prototype.pop;
+  popToRoot   : typeof NavigatorView.prototype.popToRoot;
+  removeRoute : typeof NavigatorView.prototype.removeRoute;
+  removeRoutes: typeof NavigatorView.prototype.removeRoutes;
+  replaceRoute: typeof NavigatorView.prototype.replaceRoute;
+  insertRoute : typeof NavigatorView.prototype.insertRoute;
+  setRoutes   : typeof NavigatorView.prototype.setRoutes;
 
-  setNavigationBarHidden?: typeof NavigatorView.prototype.setNavigationBarHidden;
+  setNavigationBarHidden: typeof NavigatorView.prototype.setNavigationBarHidden;
 
   // convenience navigator commands
-  replacePreviousRoute?: typeof NavigatorView.prototype.replacePreviousRoute;
-  replaceCurrentRoute ?: typeof NavigatorView.prototype.replaceCurrentRoute;
-  removePreviousRoute ?: typeof NavigatorView.prototype.removePreviousRoute;
-  removeAllPrevRoutes ?: typeof NavigatorView.prototype.removeAllPrevRoutes;
+  replacePreviousRoute: typeof NavigatorView.prototype.replacePreviousRoute;
+  replaceCurrentRoute : typeof NavigatorView.prototype.replaceCurrentRoute;
+  removePreviousRoute : typeof NavigatorView.prototype.removePreviousRoute;
+  removeAllPrevRoutes : typeof NavigatorView.prototype.removeAllPrevRoutes;
 
   // misc. navigator commands
-  sendCustomCommand?: typeof NavigatorView.prototype.sendCustomCommand;
+  sendCustomCommand: typeof NavigatorView.prototype.sendCustomCommand;
 
   // route commands
-  getRouteOptions   ?: typeof NavigatorRouteView.prototype.getRouteOptions;
-  setRouteOptions   ?: typeof NavigatorRouteView.prototype.setRouteOptions;
-  setHidesBackButton?: typeof NavigatorRouteView.prototype.setHidesBackButton;
+  getRouteOptions   : typeof NavigatorRouteView.prototype.getRouteOptions;
+  setRouteOptions   : typeof NavigatorRouteView.prototype.setRouteOptions;
+  setHidesBackButton: typeof NavigatorRouteView.prototype.setHidesBackButton;
 
   // get ref functions
-  getRefToRoute          ?: () => NavigatorRouteView;
-  getRefToNavigator      ?: () => NavigatorView;
-  getRefToNavRouteEmitter?: () => EventEmitter<NavRouteEvents>;
+  getRefToRoute          : () => NavigatorRouteView;
+  getRefToNavigator      : () => NavigatorView;
+  getRefToNavRouteEmitter: () => EventEmitter<NavRouteEvents>;
 };
