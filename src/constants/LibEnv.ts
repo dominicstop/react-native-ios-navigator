efe47a69bc1607e3386050ae4a517fed9a9ff926
Dominic Go <dominicstop@gmail.com>
2021-07-27 17:01:56 +0800

    💄 Minor Changes

--- src/constants/LibEnv.ts
+++ src/constants/LibEnv.ts
@@ -1,3 +1,9 @@
+// Note: Changes in this file is ignored by default.
+// * In order to commit changes run:
+//   `git update-index --no-assume-unchanged src/constants/LibEnv.ts`
+// * To ignore changes, run:
+//   `git update-index --assume-unchanged src/constants/LibEnv.ts`
+
 export const LIB_ENV = {
   /** Log debug messages to console */
   debugLog: __DEV__ && true,
